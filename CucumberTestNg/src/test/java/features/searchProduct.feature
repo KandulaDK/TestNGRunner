9e08664c2e229675ecd033feeb40304d37fb4caa
Dinnubunny <kanduladk@gmail.com>
2022-07-15 11:39:28 +0530

    b

--- CucumberTestNg/src/test/java/features/searchProduct.feature
+++ CucumberTestNg/src/test/java/features/searchProduct.feature
@@ -1,19 +1,17 @@
 Feature: Search and Place the order of the product
 
-@SearchProducts
-Scenario Outline: Search Experience for product search in both home and Offers Page
+  @SearchProducts
+  Scenario Outline: Search Experience for product search in both home and Offers Page
+    Given User is on GreenCart Landing Page
+    When User Searched with shortName <shortName> and Extracted resultes of the product
+    Then User searched for same shortname <shortName> in offers page
+    And check is both products are same or not
 
-Given User is on GreenCart Landing Page
-When User Searched with shortName <shortName> and Extracted resultes of the product
-Then User searched for same shortname <shortName> in offers page
-And check is both products are same or not
-
-
-Examples:
-  |shortName|
-	|	Tom		|
-	|	Car		|
-	|	cau		|
+    Examples: 
+      | shortName |
+      | Tom       |
+#	|	Car		|
+#	|	cau		|
 #	|	Wal		|
 #	| cas		|
 #	| bea		|
--- CucumberTestNg/src/test/java/pageObjectModel/LandingPgPOM.java
+++ CucumberTestNg/src/test/java/pageObjectModel/LandingPgPOM.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 
 public class LandingPgPOM {
 	public WebDriver driver;
-	
+
 	private By searchBox =  By.cssSelector("input[type='search']");
 	private By productName = By.xpath("//h4[@class='product-name']");
 	private By topDeals = By.partialLinkText("Top Deals");
@@ -17,50 +17,50 @@ public class LandingPgPOM {
 	private By addToCart = By.xpath("//button[.='ADD TO CART']");
 	private By bag = By.cssSelector("[alt='Cart']");
 	private By checkOut = By.xpath("//button[.='PROCEED TO CHECKOUT']");
-	
-	
+
+
 	public  LandingPgPOM(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	
+
+
 	public void searchItem(String shortName) {
 		driver.findElement(searchBox).sendKeys(shortName);
-		
+
 	}
-	
+
 	public String getTheProductName() {
 		return driver.findElement(productName).getText();
 	}
-	
-	
+
+
 	public void goTOTopDealsPg() {
 		driver.findElement(topDeals).click();
 	}
-	
+
 	public String increaseTheQuantity(String q) {
-		
+
 		while(!driver.findElement(quantity).getAttribute("value").contains(q)){
 			driver.findElement(increment).click();
 		}
 		return driver.findElement(quantity).getAttribute("value");
-		
+
 	}
-	
+
 	public void addToCart() {
 		driver.findElement(addToCart).click();
 	}
-	
+
 	public void proceedToCheckOut() {
 		driver.findElement(bag).click();
 		driver.findElement(checkOut).click();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 }
